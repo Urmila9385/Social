64db8401acad0107077e5f6dbe4b58c09685d432
Urmila9385 <79640277+Urmila9385@users.noreply.github.com>
2026-04-20 15:17:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 def get_location_url(location):
     url = f"https://nominatim.openstreetmap.org/search?q={location}&format=json"
     response = requests.get(url).json()
-    
+    print(response)
     if response:
         lat, lon = response[0]["lat"], response[0]["lon"]
         return f"https://www.openstreetmap.org/?mlat={lat}&mlon={lon}"
